a9649040386d21f1b4b71e33518570d471483b4c
Ensar Gök <ensarxgok@gmail.com>
2024-03-13 18:47:24 +0300

    refac: first implementation

--- analysis.c
+++ analysis.c
@@ -7,227 +7,360 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-typedef enum _TermType {
-    POLYNOMIAL,
+#define EVALABLE void*
+
+double evaluate(EVALABLE *e, double value);
+
+typedef enum _EvalAbleType {
+    CONSTANT,
+    VARIABLE,
+    FUNCTION,
+    LOGARITHM,
+    EXPONENTIAL,
     TRIGONOMETRIC,
-    LOGARITHMIC,
-} TermType;
+    INVERSE_TRIGONOMETRIC,
+} EvalAbleType;
 
-typedef struct _PolynomialTerm {
-    double coefficient;
-    int exponent;
-    // evaluate function
-} PolynomialTerm;
+/* Struct Prototypes */
+typedef struct _Constant Constant;
+typedef struct _Variable Variable;
+typedef struct _Function Function;
+typedef struct _Exponential Exponential;
+typedef struct _Logarithm Logarithm;
+typedef struct _Trigonometric Trigonometric;
+typedef enum _TrigonometricType TrigonometricType;
+typedef struct _InverseTrigonometric InverseTrigonometric;
 
-double evaluatePolynomial(double x, PolynomialTerm *term) {
-    return term->coefficient * pow(x, term->exponent);
+
+typedef struct _EvalAble {
+    EvalAbleType type;
+} EvalAble;
+
+typedef struct _Function Function;
+typedef struct _Constant Constant;
+typedef struct _Variable Variable;
+typedef struct _Exponential Exponential;
+
+EvalAbleType checkType(EVALABLE e) {
+    return ((EvalAble *)e)->type;
 }
 
-PolynomialTerm derivePolynomial(PolynomialTerm *term) {
-    PolynomialTerm derived;
-    derived.coefficient = term->coefficient * term->exponent;
-    derived.exponent = term->exponent - 1;
-    return derived;
-}
+typedef struct _Constant {
+    EvalAbleType type;
+    double value;
+} Constant;
 
-typedef enum _TrigonometricFunction {
+typedef struct _Variable {
+    EvalAbleType type;
+    EVALABLE *cofactor;
+} Variable;
+
+typedef struct _Function {
+    EvalAbleType type;
+    int argCount;
+    EvalAble *args[256];
+} Function;
+
+typedef struct _Exponential {
+    EvalAbleType type;
+    EVALABLE *cofactor;
+    EVALABLE *base;
+    EVALABLE *exponent;
+} Exponential;
+
+typedef enum _TrigonometricType {
     SIN,
     COS,
     TAN,
-    COT,
-    SEC,
     CSC,
-} TrigonometricFunction;
+    SEC,
+    COT,
+} TrigonometricType;
 
-typedef struct _TrigonometricTerm {
-    double coefficient;
-    TrigonometricFunction function;
-    double period;
-    double phase;
-} TrigonometricTerm;
+typedef struct _Trigonometric {
+    EvalAbleType type;
+    TrigonometricType trigType;
+    EVALABLE *cofactor;
+    EVALABLE *arg;
+} Trigonometric;
 
-double evaluateTrigonometric(double x, TrigonometricTerm *term) {
-    double value = 0;
-    switch (term->function) {
+typedef enum _InverseTrigonometricType {
+    ASIN,
+    ACOS,
+    ATAN,
+    ACSC,
+    ASEC,
+    ACOT,
+} InverseTrigonometricType;
+
+typedef struct _InverseTrigonometric {
+    EvalAbleType type;
+    InverseTrigonometricType trigType;
+    EVALABLE *cofactor;
+    EVALABLE *arg;
+} InverseTrigonometric;
+
+typedef struct _Logarithm {
+    EvalAbleType type;
+    EVALABLE *cofactor;
+    EVALABLE *base;
+    EVALABLE *exponent;
+} Logarithm;
+
+/* Function prototypes */
+Variable *createVariable(EVALABLE *cofactor);
+void destroyVariable(Variable *v);
+double evaluateVariable(Variable *v, double value);
+
+Constant *createConstant(double value);
+void destroyConstant(Constant *c);
+double evaluateConstant(Constant *c, double value);
+
+Exponential *createExponential(EVALABLE *cofactor, EVALABLE *base, EVALABLE *exponent);
+void destroyExponential(Exponential *e);
+double evaluateExponential(Exponential *e, double value);
+
+Logarithm *createLogarithm(EVALABLE *cofactor, EVALABLE *base, EVALABLE *exponent);
+void destroyLogarithm(Logarithm *l);
+double evaluateLogarithm(Logarithm *l, double value);
+
+Function *createFunction(int argCount);
+void addFunctionArg(Function *f, EvalAble *argType);
+void destroyFunction(Function *f);
+double evaluateFunction(Function *f, double value);
+
+Trigonometric *createTrigonometric(TrigonometricType type, EVALABLE *cofactor, EVALABLE *arg);
+void destroyTrigonometric(Trigonometric *t);
+double evaluateTrigonometric(Trigonometric *t, double value);
+
+InverseTrigonometric *createInverseTrigonometric(InverseTrigonometricType type, EVALABLE *cofactor, EVALABLE *arg);
+void destroyInverseTrigonometric(InverseTrigonometric *it);
+double evaluateInverseTrigonometric(InverseTrigonometric *it, double value);
+
+/* Function implementations */
+
+// Variable functions
+Variable *createVariable(EVALABLE *cofactor) {
+    Variable *v = (Variable *)malloc(sizeof(Variable));
+    v->type = VARIABLE;
+    if (cofactor == NULL) {
+        cofactor = (EVALABLE *)createConstant(1);
+    }
+    v->cofactor = cofactor;
+    return v;
+}
+
+void destroyVariable(Variable *v) {
+    free(v);
+}
+
+double evaluateVariable(Variable *v, double value) {
+    return evaluate(v->cofactor, value) * value;
+}
+
+// Constant functions
+Constant *createConstant(double value) {
+    Constant *c = (Constant *)malloc(sizeof(Constant));
+    c->type = CONSTANT;
+    c->value = value;
+    return c;
+}
+
+void destroyConstant(Constant *c) {
+    free(c);
+}
+
+double evaluateConstant(Constant *c, double value) {
+    return c->value;
+}
+
+// Exponential functions
+Exponential *createExponential(EVALABLE *cofactor, EVALABLE *base, EVALABLE *exponent) {
+    Exponential *e = (Exponential *)malloc(sizeof(Exponential));
+    e->type = EXPONENTIAL;
+    if (cofactor == NULL) {
+        cofactor = (EVALABLE *)createConstant(1);
+    }
+    e->cofactor = cofactor;
+    if (base == NULL) {
+        base = (EVALABLE *)createConstant(1);
+    }
+    e->base = base;
+    if (exponent == NULL) {
+        exponent = (EVALABLE *)createConstant(1);
+    }
+    e->exponent = exponent;
+    return e;
+}
+
+void destroyExponential(Exponential *e) {
+    free(e);
+}
+
+double evaluateExponential(Exponential *e, double value) {
+    return evaluate(e->cofactor, value) * pow(evaluate(e->base, value), evaluate(e->exponent, value));
+}
+
+// Trigonometric functions
+
+Trigonometric *createTrigonometric(TrigonometricType type, EVALABLE *cofactor, EVALABLE *arg) {
+    Trigonometric *t = (Trigonometric *)malloc(sizeof(Trigonometric));
+    t->type = TRIGONOMETRIC;
+    if (cofactor == NULL) {
+        cofactor = (EVALABLE *)createConstant(1);
+    }
+    t->cofactor = cofactor;
+    if (arg == NULL) {
+        arg = (EVALABLE *)createConstant(1);
+    }
+    t->arg = arg;
+    return t;
+}
+
+void destroyTrigonometric(Trigonometric *t) {
+    free(t);
+}
+
+double evaluateTrigonometric(Trigonometric *t, double value) {
+    switch (t->trigType) {
         case SIN:
-            value = sin(x);
-            break;
+            return evaluate(t->cofactor, value) * sin(evaluate(t->arg, value));
         case COS:
-            value = cos(x);
-            break;
+            return evaluate(t->cofactor, value) * cos(evaluate(t->arg, value));
         case TAN:
-            value = tan(x);
-            break;
-        case COT:
-            value = 1 / tan(x);
-            break;
-        case SEC:
-            value = 1 / cos(x);
-            break;
+            return evaluate(t->cofactor, value) * tan(evaluate(t->arg, value));
         case CSC:
-            value = 1 / sin(x);
-            break;
-    }
-    return term->coefficient * value;
-}
-
-TrigonometricTerm deriveTrigonometric(TrigonometricTerm *term) {
-    TrigonometricTerm derived;
-    derived.coefficient = term->coefficient;
-    switch (term->function) {
-        case SIN:
-            derived.function = COS;
-            break;
-        case COS:
-            derived.function = SIN;
-            break;
-        case TAN:
-            derived.function = SEC;
-            break;
-        case COT:
-            derived.function = -CSC;
-            break;
+            return evaluate(t->cofactor, value) * 1 / sin(evaluate(t->arg, value));
         case SEC:
-            derived.function = TAN;
-            break;
-        case CSC:
-            derived.function = -COT;
-            break;
+            return evaluate(t->cofactor, value) * 1 / cos(evaluate(t->arg, value));
+        case COT:
+            return evaluate(t->cofactor, value) * 1 / tan(evaluate(t->arg, value));
     }
-    derived.period = term->period;
-    derived.phase = term->phase + M_PI_2;
-    return derived;
 }
 
-typedef struct _LogarithmicTerm {
-    double coefficient;
-    double base;
-} LogarithmicTerm;
+// Inverse Trigonometric functions
 
-double evaluateLogarithmic(double x, LogarithmicTerm *term) {
-    return term->coefficient * log(x) / log(term->base);
+InverseTrigonometric *createInverseTrigonometric(InverseTrigonometricType type, EVALABLE *cofactor, EVALABLE *arg) {
+    InverseTrigonometric *it = (InverseTrigonometric *)malloc(sizeof(InverseTrigonometric));
+    it->type = INVERSE_TRIGONOMETRIC;
+    if (cofactor == NULL) {
+        cofactor = (EVALABLE *)createConstant(1);
+    }
+    it->cofactor = cofactor;
+    if (arg == NULL) {
+        arg = (EVALABLE *)createConstant(1);
+    }
+    it->arg = arg;
+    return it;
 }
 
-LogarithmicTerm deriveLogarithmic(LogarithmicTerm *term) {
-    LogarithmicTerm derived;
-    derived.coefficient = term->coefficient / log(term->base);
-    derived.base = term->base;
-    return derived;
+void destroyInverseTrigonometric(InverseTrigonometric *it) {
+    free(it);
 }
 
-typedef struct _Term {
-    TermType type;
-    union {
-        PolynomialTerm polynomial;
-        TrigonometricTerm trigonometric;
-        LogarithmicTerm logarithmic;
-    };
-} Term;
+double evaluateInverseTrigonometric(InverseTrigonometric *it, double value) {
+    switch (it->trigType) {
+        case ASIN:
+            return evaluate(it->cofactor, value) * asin(evaluate(it->arg, value));
+        case ACOS:
+            return evaluate(it->cofactor, value) * acos(evaluate(it->arg, value));
+        case ATAN:
+            return evaluate(it->cofactor, value) * atan(evaluate(it->arg, value));
+        case ACSC:
+            return evaluate(it->cofactor, value) * asin(1 / evaluate(it->arg, value));
+        case ASEC:
+            return evaluate(it->cofactor, value) * acos(1 / evaluate(it->arg, value));
+        case ACOT:
+            return evaluate(it->cofactor, value) * atan(1 / evaluate(it->arg, value));
+    }
+}
 
-double evaluateTerm(double x, Term *term) {
-    double value = 0;
-    switch (term->type) {
-        case POLYNOMIAL:
-            value = evaluatePolynomial(x, &term->polynomial);
-            break;
+// Logarithm functions
+
+Logarithm *createLogarithm(EVALABLE *cofactor, EVALABLE *base, EVALABLE *exponent) {
+    Logarithm *l = (Logarithm *)malloc(sizeof(Logarithm));
+    l->type = LOGARITHM;
+    if (cofactor == NULL) {
+        cofactor = (EVALABLE *)createConstant(1);
+    }
+    l->cofactor = cofactor;
+    if (base == NULL) {
+        base = (EVALABLE *)createConstant(1);
+    }
+    l->base = base;
+    if (exponent == NULL) {
+        exponent = (EVALABLE *)createConstant(1);
+    }
+    l->exponent = exponent;
+    return l;
+}
+
+void destroyLogarithm(Logarithm *l) {
+    free(l);
+}
+
+double evaluateLogarithm(Logarithm *l, double value) {
+    return evaluate(l->cofactor, value) * log(evaluate(l->base, value)) / log(evaluate(l->exponent, value));
+}
+
+// Function functions
+
+Function *createFunction(int argCount) {
+    Function *f = (Function *)malloc(sizeof(Function));
+    f->type = FUNCTION;
+    f->argCount = argCount;
+    return f;
+}
+
+void addFunctionArg(Function *f, EvalAble *arg) {
+    f->args[f->argCount++] = arg;
+}
+
+void destroyFunction(Function *f) {
+    free(f);
+}
+
+double evaluateFunction(Function *f, double value) {
+    double result = 0;
+    for (int i = 0; i < f->argCount; i++) {
+        result += evaluate((EVALABLE *)f->args[i], value);
+    }
+    return result;
+}
+
+// Main evaluation function
+double evaluate(EVALABLE *e, double value) {
+    switch (checkType(e)) {
+        case CONSTANT:
+            return evaluateConstant((Constant *)e, value);
+        case VARIABLE:
+            return evaluateVariable((Variable *)e, value);
+        case EXPONENTIAL:
+            return evaluateExponential((Exponential *)e, value);
         case TRIGONOMETRIC:
-            value = evaluateTrigonometric(x, &term->trigonometric);
-            break;
-        case LOGARITHMIC:
-            value = evaluateLogarithmic(x, &term->logarithmic);
-            break;
-    }
-    return value;
-}
-
-Term deriveTerm(Term *term) {
-    Term derived;
-    derived.type = term->type;
-    switch (term->type) {
-        case POLYNOMIAL:
-            derived.polynomial = derivePolynomial(&term->polynomial);
-            break;
-        case TRIGONOMETRIC:
-            derived.trigonometric = deriveTrigonometric(&term->trigonometric);
-            break;
-        case LOGARITHMIC:
-            derived.logarithmic = deriveLogarithmic(&term->logarithmic);
-            break;
-    }
-    return derived;
-}
-
-typedef struct _Function {
-    Term *terms;
-    int termCount;
-} Function;
-
-double evaluateFunction(double x, Function *function) {
-    double value = 0;
-    for (int i = 0; i < function->termCount; i++) {
-        value += evaluateTerm(x, &function->terms[i]);
-    }
-    return value;
-}
-
-void deriveFunction(Function *function, Function *derived) {
-    derived->termCount = function->termCount;
-    for (int i = 0; i < function->termCount; i++) {
-        derived->terms[i] = deriveTerm(&function->terms[i]);
+            return evaluateTrigonometric((Trigonometric *)e, value);
+        case INVERSE_TRIGONOMETRIC:
+            return evaluateInverseTrigonometric((InverseTrigonometric *)e, value);
+        case LOGARITHM:
+            return evaluateLogarithm((Logarithm *)e, value);
+        case FUNCTION:
+            return evaluateFunction((Function *)e, value);
     }
 }
 
-int main() 
-{
-    // f(x) = 2x^3 + 3x^2 + 5
-    // f'(x) = 6x^2 + 6x
-    PolynomialTerm term1 = {2, 3};
-    PolynomialTerm term2 = {3, 2};
-    PolynomialTerm term3 = {5, 0};
-
-    Term terms[3];
-    terms[0].type = POLYNOMIAL;
-    terms[0].polynomial = term1;
-    terms[1].type = POLYNOMIAL;
-    terms[1].polynomial = term2;
-    terms[2].type = POLYNOMIAL;
-    terms[2].polynomial = term3;
-
-    Function function;
-    function.termCount = 3;
-    function.terms = terms;
-
-    printf("f(2) = %f\n", evaluateFunction(2, &function));
-
-    Function derived;
-    derived.termCount = 3;
-    derived.terms = terms;
-    deriveFunction(&function, &derived);
-
-    printf("f'(2) = %f\n", evaluateFunction(2, &derived));
-
-    // f(x) = sin(x) + 7x^2 + 3.2
-    Term terms2[3];
-    terms2[0].type = TRIGONOMETRIC;
-    terms2[0].trigonometric.coefficient = 1;
-    terms2[0].trigonometric.function = SIN;
-    terms2[0].trigonometric.period = 2 * M_PI;
-    terms2[0].trigonometric.phase = 0;
-    terms2[1].type = POLYNOMIAL;
-    terms2[1].polynomial.coefficient = 7;
-    terms2[1].polynomial.exponent = 2;
-    terms2[2].type = POLYNOMIAL;
-    terms2[2].polynomial.coefficient = 3.2;
-    terms2[2].polynomial.exponent = 0;
-
-    Function function2;
-    function2.termCount = 3;
-    function2.terms = terms2;
-
-    double pi = M_PI;
-    printf("f(%f) = %f\n", pi, evaluateFunction(pi, &function2));
-    // expected: 7 * pi^2 + 3.2 = 7 * 9.8696 + 3.2 = 69.286 + 3.2 = 72.486 
-
+int main() {
+    // x^(sin(log_5 (x^3))
+    Variable *x = createVariable(NULL);
+    Constant *c1 = createConstant(5);
+    Exponential *x3 = createExponential(NULL, (EVALABLE *)x, (EVALABLE *)createConstant(3));
+    Logarithm *l = createLogarithm(NULL, (EVALABLE *)c1, (EVALABLE *)x3);
+    Trigonometric *s = createTrigonometric(SIN, NULL, (EVALABLE *)l);
+    Exponential *e = createExponential(NULL, (EVALABLE *)x, (EVALABLE *)s);
+    printf("%f\n", evaluate((EVALABLE *)e, 7));
     return 0;
 }
+
+
+
